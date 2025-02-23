2c1a335ab9a8373f35c6af856e8a6435e69c70fd
Satej Gandre <ihs.satej@outlook.com>
2025-02-23 17:48:06 -0500

    Create sums.h

---
+++ sums.h
@@ -0,0 +1,8 @@
+#ifndef SUMS_H
+#define SUMS_H
+#include <vector>
+using namespace std;
+double left(vector<double>& x, vector<double>& y, int n);
+double right(vector<double>& x, vector<double>& y, int n);
+double trapezoidal(vector<double>& x, vector<double>& y, int n);
+#endif // SUMS_H
